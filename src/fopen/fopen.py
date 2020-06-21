d62fc9b0b06c1c3cc61e13ab2f238881076a6061
Abhishek Singh <51530258+realsingh25@users.noreply.github.com>
2020-06-21 20:25:56 +0530

    Remove foreign comments

--- src/fopen/fopen.py
+++ src/fopen/fopen.py
@@ -1,18 +1,13 @@
 #!/usr/bin/env python
 
-# Encoding: UTF-8
-# Line Endings: LF(Unix)
-# Programming Language: Python 3
-# Syntax Standard: PEP 8
-
-
-"""Copyright (C) 2020 Powflix Inc., and its affiliates.
+"""Python Scripts Bundle for extra functionalities in Command Line Terminals.
 
 Source: https://github.com/powflix/nimo | see README for more details.
 License: GNU General Public License v3 (GPLv3) | see LICENSE for more details.
 Contribution: Wanna contribute to nimo? | see CONTRIBUTING for more details.
-"""
 
+Copyright (C) 2020 Powflix Inc., and its affiliates.
+"""
 
 import sys
 
